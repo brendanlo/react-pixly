1cba65e95ef24ade6822467ffe31b17965390be8
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-03-18 13:18:35 -0400

    working on

--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
 
 function SearchForm() {
+    const formDefaults = {
+        make: "",
+        model: ""
+    };
+
+    const [inputSearch, setInputSearch]= useState(formDefaults);
+
+    function handleSubmit(evt){
+        evt.preventDefault();
+        //Add function for searching
+        setInputSearch(formDefaults);
+    }
+
+    function handleChange(evt){
+        const { name, value } = evt.target;
+        setInputSearch(data => ({ ...data, [name]: value }));
+    }
+
     return (
         <div className="SearchForm pixly">
             <h1>Search for photos</h1>
-            <p> placeholder for search form</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    id="deviceMake"
+                    name="deviceMake"
+                    placeholder="Enter Make"
+                    value={inputSearch.make}
+                    onChange={handleChange}
+                />
+                <input
+                    id="deviceModel"
+                    name="deviceModel"
+                    placeholder="Enter Camera Model"
+                    value={inputSearch.model}
+                    onChange={handleChange}
+                />
+
+                <button type="submit">
+                    Search
+                </button>
+            </form>
         </div >
     );
 }
