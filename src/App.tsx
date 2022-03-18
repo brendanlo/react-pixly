f5d5220c3705b9d21156e72b7bedec0887eb26fd
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-03-17 20:40:56 -0400

    working on

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Routes from './Routes';
 import { BrowserRouter } from 'react-router-dom';
 import PixlyApi from './Api';
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5001/api';
 
@@ -13,25 +13,37 @@ function App() {
     const [photoList, setPhotoList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    async function getAllPhotos() {
-        const response = await axios.get(`${BASE_URL}/photos`);
-
-        // overwrites the image URL to a generated URL
-        for (let photoObj of response.data) {
-            photoObj.image_url = await axios.get(
-                `${BASE_URL}/photos/imageurl`,
-                { params: { key: photoObj.image_url } });
+    useEffect(function loadUserInfo() {
+        async function getAllPhotos() {
+            const response = await axios.get(`${BASE_URL}/photos`);
+            
+    
+            // overwrites the image URL to a generated URL
+            for (let photoObj of response.data.photos) {
+    
+                const responses = await axios.get(
+                    `${BASE_URL}/photos/imageurl`,
+                    { params: { key: photoObj.image_url } });
+                photoObj.image_url = responses.data;
+                console.log(photoObj.image_url);
+            }
+    
+            setPhotoList(response.data.photos);
+            setIsLoading(false);
         }
+      
+         getAllPhotos();
+        
+    }, []);
 
-        setPhotoList(response.data);
-        setIsLoading(false);
+    if(isLoading){
+        return <p>Loading...</p>
     }
 
-    if (isLoading) {
-        getAllPhotos();
-        return <p> Loading...</p>;
+    function updatePhotoList(newFormData){
+        PixlyApi.addPhoto(newFormData);
+        setPhotoList(photoList => ({...photoList}));
     }
-
 
     return (
         <div className="App">
@@ -39,7 +51,7 @@ function App() {
                 <NavBar />
                 <Routes
                     getPhotos={PixlyApi.getPhotos}
-                    addPhoto={PixlyApi.addPhoto}
+                    addPhoto={updatePhotoList}
                     photoList={photoList} />
             </BrowserRouter>
         </div>
