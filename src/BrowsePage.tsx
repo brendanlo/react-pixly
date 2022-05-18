291ffb8c90ff1162b001a6595bc79a47a147d3df
brendan <lobrendan@gmail.com>
2022-05-18 16:05:59 -0700

    minor tweaks to the photo display

--- src/BrowsePage.tsx
+++ src/BrowsePage.tsx
@@ -5,7 +5,7 @@ function BrowsePage({ photoList }) {
     return (
         <div className="BrowsePage pixly">
             <h1> Browse Images</h1>
-            <p> placeholder for image category lists </p>
+            <p> See photos other users have shared </p>
             <PhotoList photoList={photoList} />
         </div>
     );
